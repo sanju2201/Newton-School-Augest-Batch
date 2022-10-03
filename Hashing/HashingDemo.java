fd0978e11d47062dfdc7e54025f2b610a675c121
SanjuManna <sanjumanna2201@gmail.com>
2022-10-03 17:17:51 +0530

    Hasing 1st Question

---
+++ Hashing/HashingDemo.java
@@ -0,0 +1,31 @@
+package Hashing;
+
+import java.util.*;
+import java.util.Iterator;
+
+public class HashingDemo {
+    public static void main(String[] args) {
+        HashMap<Integer, String> ht = new HashMap<Integer, String>();
+        ht.put(1, "L");
+        ht.put(2, "u");
+        ht.put(3, "c");
+        ht.put(4, "k");
+
+        System.out.println(ht);
+
+        System.out.println(ht.get(2));
+
+        ht.remove(4);
+        System.out.println(ht);
+
+        Iterator<Integer> itr = ht.keySet().iterator();
+        while (itr.hasNext()) {
+            Integer key = itr.next();
+            String mapValue = ht.get(key);
+            System.out.println("KEY: " + key + " , VALUE: " + mapValue);
+
+        }
+
+    }
+
+}
