c448ada65b8aa9815b218ffb7971ffb5b6b2c316
SanjuManna <sanjumanna2201@gmail.com>
2022-10-06 18:33:46 +0530

    1st Commit

---
+++ MockInterview/SortArray.java
@@ -0,0 +1,28 @@
+package MockInterview;
+
+import java.util.*;
+
+public class SortArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // int a[] = { 9, 8, 7, 6, 4, 3, 2, 1 };
+        int n = sc.nextInt();
+        int a[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+        // Bubble Sort
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n - 1 - i; j++) {
+                if (a[j] > a[j + 1]) {
+                    int temp = a[j];
+                    a[j] = a[j + 1];
+                    a[j + 1] = temp;
+                }
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            System.out.print(a[i] + " ");
+        }
+    }
+}
