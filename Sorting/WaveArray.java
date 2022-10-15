16cb5a8e0cdd9b4ad1d2ac76c4cc2c60f1546043
SanjuManna <sanjumanna2201@gmail.com>
2022-10-15 18:02:35 +0530

    Commit

---
+++ Sorting/WaveArray.java
@@ -0,0 +1,62 @@
+package Sorting;
+
+import java.io.*; // for handling input/output
+import java.util.*; // contains Collections framework
+
+// don't change the name of this class
+// you can add inner classes if needed
+class WaveArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int a[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+        Arrays.sort(a);
+        int temp;
+        for (int i = 0; i < n - 1; i = i + 2) {
+            temp = a[i];
+            a[i] = a[i + 1];
+            a[i + 1] = temp;
+        }
+        for (int i = 0; i < n; i++) {
+            System.out.print(a[i] + " ");
+        }
+
+    }
+}
+
+/*
+ * Wave array
+ * easy
+ * Time Limit: 2 sec
+ * Memory Limit: 128000 kB
+ * Problem Statement
+ * Given an N size array of unique inyegers, your task is to print the array in
+ * a wave form, i. e a1 >= a2 <= a3 >= a4 <= a5.. . print the lexicographically
+ * smallest array possible.
+ * Input
+ * First line of input contains a single integer N, next line contains N space
+ * separated integers depicting the values of the array.
+ * 
+ * Constraints:-
+ * 1 < = N < = 100000
+ * 1 < = Arr[i] < = 1000000000
+ * Output
+ * Print the array in wave form as mentioned.
+ * Example
+ * Sample Input :-
+ * 5
+ * 2 1 3 5 4
+ * 
+ * Sample Output:-
+ * 2 1 4 3 5
+ * 
+ * Sample Input:-
+ * 3
+ * 1 2 3
+ * 
+ * Sample Output:-
+ * 2 1 3
+ */
