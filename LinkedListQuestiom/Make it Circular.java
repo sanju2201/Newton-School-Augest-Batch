4362a087a996ea1497a0620e2f8bd5db461c4bda
sanju manna <107359591+sanju2201@users.noreply.github.com>
2022-10-20 01:10:33 +0530

    1st Commit

---
+++ LinkedListQuestiom/Make it Circular.java
@@ -0,0 +1,20 @@
+/*
+class Node {
+    Node next;
+    int val;
+
+    Node(int val) {
+        this.val = val;
+        next = null;
+    }
+}
+*/
+
+public static Node MakeCircular(Node head) {
+Node  temp =head;
+while(temp.next != null){
+    temp= temp.next;
+}
+temp.next = head;
+return head;
+}
