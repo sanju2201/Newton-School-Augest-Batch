37d5d282e21707299455cc3259b574c9fd1cc0a1
sanju manna <107359591+sanju2201@users.noreply.github.com>
2022-10-22 11:00:42 +0530

    2nd commit

--- LinkedListQuestiom/Add two numbers.java
+++ LinkedListQuestiom/Add two numbers.java
@@ -9,46 +9,21 @@ class Node {
     }
 }
 */
-static Node head;
-static void addLast(int data){
-  Node newNode =new Node(data);
-  if(head == null){
-    head =newNode;
-    return;
-  }
-  Node ptr= head;
-  while(ptr.next != null){
-    ptr = ptr.next;
-  }
-  ptr.next = newNode;
-}
-
   static Node addNumber(Node first, Node second){
 // return the head of the modified linked list
-int sum=0, leftSumVal =0;
-while(first != null && second != null){
-  sum = (first.data + second.data + leftSumVal)%10;
-  leftSumVal = (first.data + second.data + leftSumVal)/10;
-  addLast(sum);
-  first = first.next;
-  second = second.next;
-}
-while(first != null){
-  sum = (first.data + leftSumVal)%10;
-  leftSumVal = (first.data + leftSumVal)/10;
-  addLast(sum);
-  first = first.next;
-}
-while(second != null){
-  sum = (second.data + leftSumVal)%10;
-  leftSumVal = (second.data + leftSumVal)/10;
-  addLast(sum);
-  second = second.next;
-}
-while(leftSumVal !=0){
-  addLast(leftSumVal%10);
-  leftSumVal = leftSumVal/10;
-}
-return head;
-
+        Node prev = new Node(0);
+        Node head = prev;
+        int carry = 0;
+        while (first != null || second != null || carry != 0) {
+            Node cur = new Node(0); 
+            int sum = ((second == null) ? 0 : second.data) + ((first == null) ? 0 : first.data) + carry;
+            cur.data = sum % 10;
+            carry = sum / 10;
+            prev.next = cur;
+            prev = cur;
+            
+            first = (first == null) ? first : first.next;
+            second = (second == null) ? second : second.next;
+        }
+        return head.next;
 }
