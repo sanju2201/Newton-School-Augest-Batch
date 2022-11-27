ed106de10645aea4413ea77c7c398d5e24e10c0e
sanju manna <107359591+sanju2201@users.noreply.github.com>
2022-11-27 20:16:28 +0530

    Commit

---
+++ LinkedListQuestiom/Reversing the Singly Linked List.java
@@ -0,0 +1,30 @@
+/*
+// information about the class Nodeclass Node {
+    Node next;
+    int val;
+
+    Node(int val) {
+        this.val = val;
+        next = null;
+    }
+}
+*/
+
+
+public static Node ReverseLinkedList(Node head) {
+        Node prev = null; 
+        Node current = head; 
+        Node next = null; 
+        while (current != null) { 
+            next = current.next; 
+            current.next = prev; 
+            prev = current; 
+            current = next; 
+        } 
+        head = prev; 
+        return head; 
+		
+        
+    }
+
+// https://my.newtonschool.co/playground/code/wgmperqyjbf9/
